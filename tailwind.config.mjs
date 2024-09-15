88a417e633190224a6afb24b71ae3916e5f367b6
Ishay Rosengarten <ishay60@gmail.com>
2024-09-15 10:46:08 +0300

    shopify clone bootstrap

---
+++ tailwind.config.mjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@shadcn/ui/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
